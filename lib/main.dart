3bebe05c5e0ed084fb0185e0a7cf4bc3b33c085b
SNEHAIP <snehaprasheed12@gmail.com>
2024-10-03 10:47:18 +0530

    new one

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 void main()
 {
-  runApp(Student());
+  runApp(const MyApp());
 }
-
-
-class Student extends StatelessWidget {
-  const Student({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-   home: Scaffold(
-     appBar: AppBar(
-       title: Text("student app"),
-       backgroundColor: Colors.amber,
-       
-     ),
-   ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black26,
+          title: Text("Patient App"),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Text("PatientName"),
+              TextField(),
+              Text("Mobile No"),
+              TextField(),
+              Text("Email"),
+              TextField(),
+              Text("Address"),
+              TextField(),
+              Text("PinCode"),
+              TextField(),
+              ElevatedButton(onPressed: (){
+
+              },child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
